2c21c2e71003f4225c93f31dcb4f226f23593edb
Mateo Calcagno <mateo55_malunies@hotmail.com>
2025-10-27 18:10:37 -0300

    Base of the project
    Just a blank spring boot project.

---
+++ src/main/java/matepore/email/sender/SpringBootEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package matepore.email.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailSenderApplication.class, args);
+	}
+
+}
